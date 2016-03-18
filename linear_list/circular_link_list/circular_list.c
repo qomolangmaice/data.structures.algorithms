5b6bf069b9e67939e8ad270954d567cd7dce4094
qomolangmaice <qomolangmaice@163.com>
2016-03-18 21:38:32 +0800

    Create circular link list project

---
+++ linear_list/circular_link_list/circular_list.c
@@ -0,0 +1,106 @@
+/*
+* File Name: circular_list.c
+* Description: circular link list 
+* Author: iczelion
+* Email: qomolangmaice@163.com 
+* Created: 2016.03.18 19:42:33
+*/
+
+node* _malloc_node(circular_list *list, elem_type val)
+{
+	node *s = (node*)malloc(sizeof(node));
+	assert(s != NULL);
+	s->data = val;
+	s->next = NULL;
+	return s;
+}
+
+status init_list(circular_list *list)
+{
+	node *s = (node*)malloc(sizeof(node));
+	assert(s != NULL);
+	list->head = list->tail = s;
+	list->tail->next = list->head;
+	list->size = 0;
+	return OK;
+}
+
+status push_back(circular_list *list, elem_type item)
+{
+	node *s = _malloc_node(list, item);
+	list->tail->next = s;
+	list->tail= s;
+	list->tail->next = list->head;
+	list->size++;
+	return OK;
+}
+
+status show_list(circular_list *list)
+{
+	node *p = list->head->next;
+	while(p != list->head)
+	{
+		printf("%d-->", p->data);
+		p = p->next;
+	}
+	printf("Null.\n");
+}
+
+status push_front(circular_list *list, elem_type item)
+{
+	node *s = _malloc_node(list, item);
+	s->next = list->head->next;
+	list->head->next = s;
+	if(list->head == list->tail)
+		list->tail = s;
+	list->size++;
+	return OK;
+}
+
+status pop_back(circular_list *list)
+{
+	if(list->size == 0)
+		return ERROR;
+	node *p = list->head;
+	while(p->next != list->tail)
+		p = p->next;
+
+	free(list->tail);
+	list->tail = p;
+	list->tail->next = list->head;
+	list->size--;
+	return ERROR;
+}
+
+status pop_front(circular_list *list)
+{
+	if(list->size == 0)
+		return ERROR;
+	node *p = list->head->next;
+	list->head->next = p->next;
+	free(p);
+	if(list->size == 1)
+		list->tail = list->head;
+	list->size--;
+	return OK;
+}
+
+status insert_val(circular_list *list, elem_type val)
+{
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ linear_list/circular_link_list/circular_list.h
@@ -0,0 +1,51 @@
+/*
+* File Name: circular_list.h
+* Description: 
+* Author: iczelion
+* Email: qomolangmaice@163.com 
+* Created: 2016.03.18 19:41:55
+*/
+
+#ifndef __CIRCULAR_LIST__
+#define __CIRCULAR_LIST__
+#include <stdio.h>
+#include <malloc.h>
+#include <assert.h>
+
+#define OK 1
+#define ERROR 0
+
+typedef int elem_type;
+typedef int status;
+
+typedef struct node {
+	elem_type data;
+	struct node *next;
+} node, *p_node;
+
+typedef struct circular_list{
+	p_node head;
+	p_node tail;
+	size_t size;
+} circular_list;
+
+node* _malloc_node(circular_list *list, elem_type val);
+status init_list(circular_list *list);
+status push_back(circular_list *list, elem_type item);
+status show_list(circular_list *list);
+status push_front(circular_list *list, elem_type item);
+status pop_back(circular_list *list);
+status pop_front(circular_list *list);
+status insert_val(circular_list *liste, elem_type val);
+node* find(circular_list *list, elem_type key);
+int get_length(circular_list *list);
+status delete_val(circular_list *list, elem_type key);
+status sort(circular_list *list);
+status resver(circular_list *list);
+status clear(circular_list *list);
+status destroy(circular_list *list);
+
+node* prior_node(circular_list *list, elem_type val);
+node* next_node(circular_list *list, elem_type val);
+
+#endif
