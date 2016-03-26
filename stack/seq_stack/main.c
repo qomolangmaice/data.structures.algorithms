5396ab76a0eccf011a34c5415f8fcfc4825039f8
qomolangmaice <qomoalangmaice@163.com>
2016-03-26 16:16:46 +0800

    Add stack project

---
+++ stack/seq_stack/main.c
@@ -0,0 +1,37 @@
+/*
+ *     File Name: main.c
+ *     Description: 
+ *     Author: iczelion
+ *     Email: qomolangmaice@163.com 
+ *     Created: 2016.03.26 13:55:12
+ */
+
+#include "seq_stack.h"
+#include "seq_stack.c"
+
+void main()
+{
+	seq_stack mystack;
+	elem_type val;
+
+	init_stack(&mystack);
+	printf("栈初始化完毕.\n");
+
+	for(int i=0; i<10; i++)
+	{
+		push(&mystack, i);
+		printf("入栈数据元素:%d\n", i);
+	}
+
+	pop(&mystack, &val);
+	printf("出栈栈顶数据元素:%d\n", val);
+
+	get_top(&mystack, &val);
+	printf("取得最新栈顶数据元素:%d\n", val);
+
+	printf("栈的长度为:%d\n", get_length(&mystack));
+
+	show_stack(&mystack);
+	destroy_stack(&mystack);
+	printf("栈销毁完毕.\n");
+}
